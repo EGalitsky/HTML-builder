bcd5d0d57eb7b4b4be98f691a22e594b6fb57cc4
Evgeny Galitsky <e.galitsky@bk.ru>
2022-11-06 14:47:32 +0300

    feat: add copy directory script

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,28 @@
+const fs = require("fs");
+const path = require("path");
 
+async function copy() {
+  await fs.promises.mkdir(path.join(__dirname, "files-copy"), {
+    recursive: true,
+  });
+
+  const files = await fs.promises.readdir(path.join(__dirname, "files"));
+  const filesCopy = await fs.promises.readdir(
+    path.join(__dirname, "files-copy")
+  );
+
+  for (const file of filesCopy) {
+    fs.unlink(path.join(__dirname, "files-copy", `${file}`), (err) => {
+      if (err) throw err;
+    });
+  }
+
+  for (const file of files) {
+    await fs.promises.copyFile(
+      path.join(__dirname, "files", `${file}`),
+      path.join(__dirname, "files-copy", `${file}`)
+    );
+  }
+}
+
+copy();
